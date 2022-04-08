afbed56caeffc89747ade8eb971515cefcbd1aa9
Cyrus McCormick <cyrusmcc@hotmail.com>
2022-04-07 19:43:25 -0600

    Add readConfig & buildSim functions

--- src/entities/Agent.java
+++ src/entities/Agent.java
@@ -1,14 +1,33 @@
 package entities;
 
 
+import java.util.concurrent.BlockingQueue;
+
 // Each agent should be responsible for finding its neighbors
 public class Agent implements Runnable {
 
     private Status status;
 
+    private BlockingQueue<Status> neighborStatuses;
+
+    public Agent() {
+    }
+
     @Override
     public void run() {
 
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
+    }
+
+    public void setNeighborStatuses(BlockingQueue<Status> neighborStatuses) {
+        this.neighborStatuses = neighborStatuses;
     }
 
     @Override
